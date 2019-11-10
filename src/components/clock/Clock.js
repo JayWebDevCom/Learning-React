3008588a8ce02990bee944319ea2e86252f67340
JayWebDevCom <jaiye.s@gmail.com>
2019-11-10 21:36:00 +0000

    Implement declarative Clock component

--- src/components/clock/Clock.js
+++ src/components/clock/Clock.js
@@ -1,10 +1,96 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 
-export const Clock = () =>
-    <div>
-        <div><Link to="/">Home</Link></div>
-        <div>
-            This is clock
-        </div>
-    </div>;
+export class Clock extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: "time loading..."
+        };
+    }
+
+    oneSecond = () => 1000;
+    getCurrentTime = () => new Date();
+    show = (time) => {
+        this.setState({time});
+    };
+
+    compose = (...fns) => (arg) =>
+        fns.reduce((composed, f) => f(composed), arg);
+
+    serializeClockTime = date => ({
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds()
+    });
+
+    civilianHours = clockTime => ({
+        ...clockTime,
+        hours: (clockTime.hours > 12) ? clockTime.hours - 12 : clockTime.hours
+    });
+
+    appendAMPM = clockTime => ({
+        ...clockTime,
+        ampm: (clockTime.hours > 12) ? 'PM' : 'AM'
+    });
+
+    display = target => time => target(time);
+
+    formatClock = format => time =>
+        format.replace('hh', time.hours)
+            .replace('mm', time.minutes)
+            .replace('ss', time.seconds)
+            .replace('tt', time.ampm);
+
+    prependZero = key => clockTime => ({
+        ...clockTime,
+        [key]: clockTime[key] < 10 ?
+            "0" + clockTime[key] :
+            clockTime[key]
+    });
+
+    convertToCivilianTime = clockTime =>
+        this.compose(
+            this.appendAMPM,
+            this.civilianHours
+        )(clockTime);
+
+    doubleDigits = civilianTime =>
+        this.compose(
+            this.prependZero("hours"),
+            this.prependZero("minutes"),
+            this.prependZero("seconds"),
+        )(civilianTime);
+
+
+    startTicking = () =>
+        setInterval(
+            this.compose(
+                this.getCurrentTime,
+                this.serializeClockTime,
+                this.convertToCivilianTime,
+                this.doubleDigits,
+                this.formatClock("hh:mm:ss tt"),
+                this.display(this.show)
+            ),
+            this.oneSecond()
+        );
+
+
+    componentWillMount() {
+        this.startTicking()
+    }
+
+    render() {
+        return (
+            <div>
+                <Link to="/">Home</Link>
+                <h2>The time is:</h2>
+                <div>
+                    <h3>{this.state.time}</h3>
+                </div>
+            </div>
+        )
+    }
+}
