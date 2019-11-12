a9e9fe4112815bc361977d3c9e12e3e2f3236ced
JayWebDevCom <jaiye.s@gmail.com>
2019-11-12 21:13:04 +0000

    Introduce Lifecycle methods to Color component

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {StarRating} from "./StarRating";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-export const Color = ({title, color, rating=0, onRemove=f=>f, onRate=f=>f}) =>
-    <section className={'color'}>
-        <h1>{title}</h1>
-        <button onClick={onRemove}>X</button>
-        <div className={'color'}
-             style={{backgroundColor: color}}
-        >
-        </div>
-        <div>
-            <StarRating starsSelected={rating} onRate={onRate} />
-        </div>
-    </section>;
+export class Color extends Component {
+
+    componentWillMount() {
+        this.style = {
+            backgroundColor: '#CCC'
+        }
+    }
+
+    componentWillUpdate(nextProps, nextState, nextContext) {
+        this.style = null
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        const {rating} = this.props;
+        return rating !== nextProps.rating
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        const {title, rating} = this.props;
+        const status = (rating > prevProps.rating) ? 'better' : 'worse' ;
+        console.log(`${title} is getting a ${status}`)
+    }
+
+    render() {
+        const {title, color, rating = 0, onRemove = f => f, onRate = f => f} = this.props;
+        return (
+            <section className={'color'} style={this.style}>
+                <h1 ref={"title"}>{title}</h1>
+                <button onClick={onRemove}>X</button>
+                <div className={'color'}
+                     style={{backgroundColor: color}}
+                >
+                </div>
+                <div>
+                    <StarRating starsSelected={rating} onRate={onRate}/>
+                </div>
+            </section>
+        )
+    }
+}
 
 Color.propTypes = {
     title: PropTypes.string.isRequired,
@@ -21,4 +49,11 @@ Color.propTypes = {
     rating: PropTypes.number,
     onRemove: PropTypes.func,
     onRate: PropTypes.func
+};
+
+Color.defaultProps = {
+    title: undefined,
+    rating: 0,
+    color: "#000000",
+    onRate: f => f
 };
