ab2aaeda6bb96d128fce1f85cdd892d6b2d2d8d1
JayWebDevCom <jaiye.s@gmail.com>
2019-12-21 00:40:02 +0000

    Refactor

--- src/test/components/colorPresenter.test.js
+++ src/test/components/colorPresenter.test.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {ColorPresenter} from "../../components/colourRating/ColorPresenter";
+import React from 'react';
+import {ColorPresenter} from '../../components/colourRating/ColorPresenter';
 import {mount} from 'enzyme';
 import {expect} from 'chai';
 
@@ -11,6 +11,8 @@ describe('ColorPresenter', () => {
      */
 
     const wrapper = mount(<ColorPresenter/>);
+    const testName = 'New color name';
+    const testColor = '#ff4444';
 
     it('displays colors from the store', () => {
         expect(wrapper.find('.color-list').find('section')).to.have.lengthOf(3);
@@ -19,19 +21,19 @@ describe('ColorPresenter', () => {
     it('adds colors', () => {
         expect(wrapper.find('.color-list').find('section')).to.have.lengthOf(3);
 
-        const colorForm = wrapper.find("form#add-color-form");
+        const colorForm = wrapper.find('form#add-color-form');
         const textInput = colorForm.find("input[type='text']");
         const colorInput = colorForm.find("input[type='color']");
 
-        textInput.instance().value = "New color name";
-        colorInput.instance().value = '#ff4444';
+        textInput.instance().value = testName;
+        colorInput.instance().value = testColor;
         colorForm.simulate('submit');
 
         const updatedColorList = wrapper.find('.color-list').find('section');
         expect(updatedColorList).to.have.lengthOf(4);
 
-        expect(updatedColorList.last().find('h1').first().text()).to.equal("New color name");
-        expect(updatedColorList.last().find("div.color").first().props()['style']['backgroundColor']).to.equal('#ff4444');
+        expect(updatedColorList.last().find('h1').first().text()).to.equal(testName);
+        expect(updatedColorList.last().find('div.color').first().props()['style']['backgroundColor']).to.equal(testColor);
     });
 
     it('removes a specific color on corresponding button click', () => {
@@ -39,7 +41,7 @@ describe('ColorPresenter', () => {
         const length = colorList.length;
 
         const colorEntryToRemove = colorList.filterWhere(section =>
-            section.find("div.color").first().props()['style']['backgroundColor'] === '#ff4444'
+            section.find('div.color').first().props()['style']['backgroundColor'] === testColor
         );
 
         colorEntryToRemove.first().find('button').first().simulate('click');
