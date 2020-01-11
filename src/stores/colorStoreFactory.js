657ed99b251b3e833303b73dea3d9a1414f32a07
JayWebDevCom <jaiye.s@gmail.com>
2020-01-11 16:58:24 +0000

    Delete unused quietLogger

--- src/stores/colorStoreFactory.js
+++ src/stores/colorStoreFactory.js
@@ -2,17 +2,6 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import {colors} from "../reducers/colorsReducer";
 import {sort} from "../reducers/colorsSortReducer";
 
-const debugLogger = store => next => action => {
-    let result;
-    console.groupCollapsed("dispatching", action.type);
-    console.log('previous state', store.getState());
-    console.log('action', action);
-    result = next(action);
-    console.log('next state', store.getState());
-    console.groupEnd();
-    return result
-};
-
 export const quietLogger = store => next => action =>
     next(action);
 
